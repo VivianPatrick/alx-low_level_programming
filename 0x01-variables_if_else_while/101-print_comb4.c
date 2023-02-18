378db4361ffc7b039b62886d6bb5534cff6a756b
VivianPatrick <vivianpatrick458@gmail.com>
2023-02-17 20:30:07 -0800

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: Write a program that prints all combinations of three digits
+ * Return: Always 0
+ */
+int main(void)
+{
+	int x = '0';
+	int y = '0';
+	int z = '0';
+
+	for (z = '0'; z <= '9'; z++)
+	{
+		for (y = '0'; y <= '9'; y++)
+		{
+			for (x = '0'; x <= '9'; x++)
+			{
+				if (!((x == y) || (y == z) || (y > x) || (z > y)))
+				{
+					putchar(z);
+					putchar(y);
+					putchar(x);
+					if (!(x == '9' && z == '7' && y == '8'))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
