3a95f739f054c7df48d4f235254b2fefeaad9d3e
VivianPatrick <vivianpatrick458@gmail.com>
2023-02-27 11:38:28 -0800

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+/**
+ * print_rev - Prints a string in reverse
+ * @s: The string to print
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int c = 0;
+
+	while (s[c] != '\0')
+	{
+		c++;
+	}
+
+	for (c -= 1; c >= 0; c--)
+	{
+		putchar(s[c]);
+	}
+	_putchar('\n');
+}
