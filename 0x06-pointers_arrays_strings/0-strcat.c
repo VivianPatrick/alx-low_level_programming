51b9ba00c24b9156128ea36ff71824facd216b42
VivianPatrick <vivianpatrick458@gmail.com>
2023-03-02 09:55:27 -0800

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenate two strings
+ * @destination: copy to destination
+ * @source: copy from source
+ * Return: pointer to destination
+ */
+char *_strcat(char *destination, char *source)
+{
+	int x;
+	int y;
+
+	x = 0;
+	while (destination[x] != '\0')
+	{
+		x++;
+	}
+	y = 0;
+	while (source[y] != '\0')
+	{
+		destination[x] = source[y];
+		x++;
+		y++
+	}
+	destination[x] = '\0';
+	return (destination);
+}
